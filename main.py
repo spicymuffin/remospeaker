db9ccc44a5c2aafa3eca86dbf271ce822875673c
spicymuffin <luigicussigh59@gmail.com>
2022-06-29 20:12:42 +0300

    return

--- main.py
+++ main.py
@@ -61,60 +61,11 @@ class RemoSpeaker(BoxLayout):
         self.io_history.remove_widget(work_around)
 
 
-
-print("############################## PYTHON ACTIVITY BELOW ##############################")
-from jnius import autoclass
-PythonActivity = autoclass('org.kivy.android.PythonActivity')
-print(type(PythonActivity))
-
-
-import jnius
-Context = jnius.autoclass('android.content.Context')
-Intent = jnius.autoclass('android.content.Intent')
-PendingIntent = jnius.autoclass('android.app.PendingIntent')
-AndroidString = jnius.autoclass('java.lang.String')
-NotificationBuilder = jnius.autoclass('android.app.Notification$Builder')
-Notification = jnius.autoclass('android.app.Notification')
-service_name = 'Worker'
-package_name = Context.getApplicationContext()
-PythonService = jnius.autoclass('org.kivy.android.PythonService')
-print(type(PythonService))
-service = PythonService.mService
-print(type(service))
-# Previous version of Kivy had a reference to the service like below.
-#service = jnius.autoclass('{}.Service{}'.format(package_name, service_name)).mService
-PythonActivity = jnius.autoclass('org.kivy.android' + '.PythonActivity')
-notification_service = service.getSystemService(
-    Context.NOTIFICATION_SERVICE)
-app_context = service.getApplication().getApplicationContext()
-notification_builder = NotificationBuilder(app_context)
-title = AndroidString("EzTunes".encode('utf-8'))
-message = AndroidString("Ready to play music.".encode('utf-8'))
-app_class = service.getApplication().getClass()
-notification_intent = Intent(app_context, PythonActivity)
-notification_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
-    Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK)
-notification_intent.setAction(Intent.ACTION_MAIN)
-notification_intent.addCategory(Intent.CATEGORY_LAUNCHER)
-intent = PendingIntent.getActivity(service, 0, notification_intent, 0)
-notification_builder.setContentTitle(title)
-notification_builder.setContentText(message)
-notification_builder.setContentIntent(intent)
-Drawable = jnius.autoclass("{}.R$drawable".format(service.getPackageName()))
-icon = getattr(Drawable, 'icon')
-notification_builder.setSmallIcon(icon)
-notification_builder.setAutoCancel(True)
-new_notification = notification_builder.getNotification()
-#Below sends the notification to the notification bar; nice but not a foreground service.
-#notification_service.notify(0, new_noti)
-service.startForeground(1, new_notification)
-
 class RemoSpeakerApp(App):
     def on_start(self):
-        pass
-
-
-
+        if platform == 'android':
+            self.start_service()
+            print('service started')
 
     def start_service(self):
         from jnius import autoclass
---
+++ service.py
@@ -0,0 +1,14 @@
+import kivy
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.widget import Widget 
+from jnius import autoclass
+import time
+
+
+PythonService = autoclass('org.kivy.android.PythonService')
+PythonService.mService.setAutoRestartService(True)
+
+while True:
+    print('this is my service and its running')
+    time.sleep(1)
